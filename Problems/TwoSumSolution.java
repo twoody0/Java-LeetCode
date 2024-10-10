6b806fe32e7e3fed1423a4623942eb3418bebe4e
Windeko <tylerjameswoody@gmail.com>
2024-10-10 10:13:03 -0700

    Refactored twoSum classes into a single java class

--- Problems/TwoSumSolution.java
+++ Problems/TwoSumSolution.java
@@ -3,26 +3,8 @@ package Problems;
 import java.util.HashMap;
 import java.util.Map;
 
-//O(N^2) time complexity
-//O(1) space complexity
-class BruteForceSolution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        for (int i = 0; i < nums.length; i++){
-            for (int j = i + 1; j < nums.length; j++){
-                if (nums[i] + nums[j] == target){
-                    result[0] = i;
-                    result[1] = j;
-                }
-            }
-        }
-        return result;
-    }
-}
 
-//O(N) time complexity
-//O(N) space complexity
-class OptimalSolution {
+class TwoSumSolution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
 
@@ -37,4 +19,20 @@ class OptimalSolution {
 
         throw new IllegalArgumentException("No two sum solution");
     }
+    //O(N) time complexity
+    //O(N) space complexity
+    public int[] twoSumBruteForce(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i + 1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+    //O(N^2) time complexity
+    //O(1) space complexity
 }
