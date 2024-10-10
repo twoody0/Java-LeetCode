ab5b80618478539e6af34f4f66b5db719f8cdfae
Windeko <tylerjameswoody@gmail.com>
2024-10-10 10:14:24 -0700

    Changed access modifier to public

--- Problems/TwoSumSolution.java
+++ Problems/TwoSumSolution.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-class TwoSumSolution {
+public class TwoSumSolution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
 
